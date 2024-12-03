1bb653251787244fc9dd68bbaaf9cab96c2a364a
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-12-03 23:00:41 +0200

    delete

--- dist/users/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.entity';
-import { Repository } from 'typeorm';
-export declare class UsersService {
-    private usersRepository;
-    constructor(usersRepository: Repository<User>);
-    findByUsername(username: string): Promise<User | undefined>;
-    findById(id: number): Promise<User | undefined>;
-    create(username: string, password: string): Promise<User>;
-}
--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LoginDto {
-  @ApiProperty({ example: 'username123', description: 'Имя пользователя' })
-  username: string;
-
-  @ApiProperty({ example: 'password123', description: 'Пароль пользователя' })
-  password: string;
-}
